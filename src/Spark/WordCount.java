56c0321a9bdea1bbebb7bbca3f5fd89f2f9e76b5
Elliott Chen <596620726@qq.com>
2017-11-20 16:06:11 -0600

    可以用maven编译两种文件
    pom.xml
    43行用于编译打包wordcount
    44行用于编译打包Client

--- src/Spark/WordCount.java
+++ src/Spark/WordCount.java
@@ -1,87 +1,30 @@
 package Spark;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import org.apache.spark.api.java.*;
-import org.apache.spark.api.java.function.*;
 import org.apache.spark.SparkConf;
-import scala.Tuple2;
 
 /**
  *
  * Created by xuzhuchen on 11/19/17.
  */
-public class JavaWordCount {
+/* SimpleApp.java */
+import org.apache.spark.api.java.*;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
+
+public class WordCount {
     public static void main(String[] args) {
+        String logFile = "Data/Data10_1000.csv"; // Should be some file on your system
+        SparkConf conf = new SparkConf().setAppName("CountData");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> logData = sc.textFile(logFile).cache();
 
-        // create Spark context with Spark configuration
-        JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("Spark Count"));
+        long numRows = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains(","); }
+        }).count();
 
-        // get threshold
-        final int threshold = Integer.parseInt(args[1]);
+        System.out.println("There are "+numRows+" rows.");
 
-        // read in text file and split each document into words
-        JavaRDD<String> tokenized = sc.textFile(args[0]).flatMap(
-                new FlatMapFunction() {
-                    public Iterable call(String s) {
-                        return Arrays.asList(s.split(" "));
-                    }
-                }
-        );
-
-        // count the occurrence of each word
-        JavaPairRDD<String, Integer> counts = tokenized.mapToPair(
-                new PairFunction() {
-                    public Tuple2 call(String s) {
-                        return new Tuple2(s, 1);
-                    }
-                }
-        ).reduceByKey(
-                new Function2() {
-                    public Integer call(Integer i1, Integer i2) {
-                        return i1 + i2;
-                    }
-                }
-        );
-
-        // filter out words with fewer than threshold occurrences
-        JavaPairRDD<String, Integer> filtered = counts.filter(
-                new Function, Boolean>() {
-            public Boolean call(Tuple2 tup) {
-                return tup._2 >= threshold;
-            }
-        }
-    );
-
-        // count characters
-        JavaPairRDD<Character, Integer> charCounts = filtered.flatMap(
-                new FlatMapFunction<Tuple2<String, Integer>, Character>() {
-                    @Override
-                    public Iterable<Character> call(Tuple2<String, Integer> s) {
-                        Collection<Character> chars = new ArrayList<Character>(s._1().length());
-                        for (char c : s._1().toCharArray()) {
-                            chars.add(c);
-                        }
-                        return chars;
-                    }
-                }
-        ).mapToPair(
-                new PairFunction<Character, Character, Integer>() {
-                    @Override
-                    public Tuple2<Character, Integer> call(Character c) {
-                        return new Tuple2<Character, Integer>(c, 1);
-                    }
-                }
-        ).reduceByKey(
-                new Function2<Integer, Integer, Integer>() {
-                    @Override
-                    public Integer call(Integer i1, Integer i2) {
-                        return i1 + i2;
-                    }
-                }
-        );
-
-        System.out.println(charCounts.collect());
+        sc.stop();
     }
 }
